0ee3732e95b761cf88c55afef278710e3bd15245
Toyota Sprinter Trueno <urusekoi@gmail.com>
2021-04-01 19:50:18 +0300

    Added text color change

--- lib/randomColors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
-
-Color randomColor = getRandomColor();
-
-Color getRandomColor() =>
-    Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
